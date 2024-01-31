46f7e08eec834bd92741502bebb71263d716c823
Calvin Lapp <lapp.cd@gmail.com>
2024-01-30 20:26:47 -0500

    Seed admin migration

---
+++ migrations/20240131012304_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '7e3663dd-ae72-447e-919a-394a40060fc1',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$qf9cBkewxvqmhWaEZY7SHQ$WYN8GA8QgAEhaC/r25Aaivbgek/Vn0SJw6BFLRT8mws'
+)
