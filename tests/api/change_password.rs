607013403a6ebb1391fd6e56777423414adec9c8
Calvin Lapp <lapp.cd@gmail.com>
2024-01-31 20:11:20 -0500

    Happy change password

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -15,7 +15,8 @@ async fn you_must_be_logged_in_to_change_your_password() {
 
     let res = app
         .post_change_password_body(&serde_json::json!({
-            "current_password": Uuid::new_v4().to_string(),"new_password": &new_password,
+            "current_password": Uuid::new_v4().to_string(),
+            "new_password": &new_password,
             "new_password_check": &new_password
         }))
         .await;
@@ -72,4 +73,42 @@ async fn current_password_must_be_valid() {
 
     let html_page = app.get_change_password_html().await;
     assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"));
+}
+
+#[tokio::test]
+async fn changing_password_works() {
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    });
+    let res = app.post_login(&login_body).await;
+    assert_is_redirect_to(&res, "/admin/dashboard");
+
+    let res = app
+        .post_change_password_body(&serde_json::json!({
+            "current_password": app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &new_password
+        }))
+        .await;
+    assert_is_redirect_to(&res, "/admin/password");
+
+    let html = app.get_change_password_html().await;
+    assert!(html.contains("<p><i>Your password has been changed.</i></p>"));
+
+    let res = app.post_logout().await;
+    assert_is_redirect_to(&res, "/login");
+
+    let html = app.get_login_html().await;
+    assert!(html.contains("<p><i>You have successfully logged out.</i></p>"));
+
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &new_password,
+    });
+    let res = app.post_login(&login_body).await;
+    assert_is_redirect_to(&res, "/admin/dashboard");
 }
